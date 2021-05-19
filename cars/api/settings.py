00968aff3e2cb6b67ac61fcea50c690deb7aefb7
Vlado Chernenko <vladimir.chernenko@kiwi.com>
2021-05-19 09:36:39 +0200

    ci / cd: add github workflows

--- cars/api/settings.py
+++ cars/api/settings.py
@@ -1,7 +1,8 @@
 import os
 
+DB_HOST = os.environ.get("DB_HOST", "0.0.0.0")
 DB_USER = os.environ["DB_USER"]
 DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
 DB_NAME = os.environ["DB_NAME"]
 
-DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@0.0.0.0:5432/{DB_NAME}"
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
